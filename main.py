fe7093479efde798cad74d8d94b76358b959b71a
laqq6 <112838641+laqq6@users.noreply.github.com>
2022-09-05 16:32:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -47,9 +47,9 @@ def get_weather(region):
         print("推送消息失败，请检查和风天气key是否正确！")
         os.system("pause")
         sys.exit(1)
-  else:
+    else:
         # 获取地区的location--id
-       location_id = response["location"][0]["id"]
+        location_id = response["location"][0]["id"]
     weather_url = "https://devapi.qweather.com/v7/weather/now?location={}&key={}".format(location_id, key)
     indices_url = "https://devapi.qweather.com/v7/indices/1d?type=1,2&location={}&key={}".format(location_id, key)
     response = get(weather_url, headers=headers).json()
