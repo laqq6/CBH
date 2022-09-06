72060f011ec41fb008e4ee2987f6ecbb058bb1c6
laqq6 <112838641+laqq6@users.noreply.github.com>
2022-09-06 09:50:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def get_weather(region):
     text2 = gg["daily"][1]["text"]
     warning_url = "https://devapi.qweather.com/v7/warning/now?location={}&key={}".format(location_id, key)
     hh = get(warning_url, headers=headers).json()
-    text3 = hh["warning"][0]["text"]
+    text3 = hh["warning"]["text"]
     return weather, temp, wind_dir, text1, text2, text3
 
 
